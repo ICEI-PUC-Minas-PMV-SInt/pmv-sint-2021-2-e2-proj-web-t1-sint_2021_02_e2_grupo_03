97bff618fd2c423993716f7ea597456bfe532549
unknown <flaviospjunior@gmail.com>
2021-11-25 14:58:34 -0300

    Ajustes na página de mapa de vagas

--- src/src/pages/MapaVagas/MapaVagas.jsx
+++ src/src/pages/MapaVagas/MapaVagas.jsx
@@ -5,34 +5,43 @@ import P from 'components/P';
 import Vaga from 'components/Vaga/Vaga';
 import { FaCar } from 'react-icons/fa';
 
-
-const jsonTeste = [{
-  vaga:0
+const jsonTeste = [
+  {
+    vaga: 4,
   },
   {
-    vaga: 1
-    },
-    {
-      vaga: 2,
-      },
-      { }]
+    vaga: 1,
+  },
+  {
+    vaga: 6,
+  },
+  {
+    vaga: 12,
+  },
+  { vaga: 13 },
+  { vaga: 3 },
+];
 
-
-const arrayteste = []
-jsonTeste.map((elemento,index) => ( arrayteste[elemento.vaga]=elemento.vaga ));
-console.log([...arrayteste]);
-
-
+const numeroVagas = 50;
 
 const MapaVagas = () => (
   <React.Fragment>
-    <Box style= {{
-      display:'grid',
-      gridTemplateColumns: 'repeat(10,40px)',
-      justifyContent:'center',
-      paddingTop:'100px'
-    }} >
-    {[...arrayteste].map( (elemento,index) => (<Vaga key={index} texto={index} ocupado ={jsonTeste.find( (item) => item.vaga=== index)   }/>))} </Box>
+    <Box
+      style={{
+        display: 'grid',
+        gridTemplateColumns: 'repeat(10,40px)',
+        justifyContent: 'center',
+        paddingTop: '100px',
+      }}
+    >
+      {[...Array(numeroVagas)].map((elemento, index) => (
+        <Vaga
+          key={index}
+          texto={index + 1}
+          ocupado={jsonTeste.find((item) => item.vaga === index + 1)}
+        />
+      ))}
+    </Box>
   </React.Fragment>
 );
 
